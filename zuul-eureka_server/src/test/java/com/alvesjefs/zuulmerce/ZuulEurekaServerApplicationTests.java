c496114b8b1a61896dfab97553bb0b9e86e4b7bc
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-01-14 21:24:26 -0300

    Zuul Eureka Server

---
+++ zuul-eureka_server/src/test/java/com/alvesjefs/zuulmerce/ZuulEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.zuulmerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
